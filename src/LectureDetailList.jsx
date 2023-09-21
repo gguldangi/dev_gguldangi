354e1527d35a702a0864bdeaec851fd5815bf454
YeongJae0114 <yjl8628@naver.com>
2023-09-21 11:25:05 +0900

    :sparkles: Category add
    카테고리별 바 생성

--- src/LectureDetailList.jsx
+++ src/LectureDetailList.jsx
@@ -1,51 +1,51 @@
 import LectureItem from './LectureItem';
+import Category from './Category';
 import './Styles.css';
 import React, { useState } from 'react';
+
+
+
 
 const LectureDetailList = ({ Lecture_list, num }) => {
     const [showFree, setShowFree] = useState(true);
 
     const filteredLectureList2 = Lecture_list.filter((it) => {
         const price = parseInt(it.price);
-        return (!showFree && price !== 0) && parseInt(it.field) === num;
+        return !showFree && price !== 0 && parseInt(it.field) === num;
     });
 
     const filteredLectureList = Lecture_list.filter((it) => {
         return parseInt(it.field) === num;
     });
-    const setShow = ()=>{
-        setShowFree(!showFree)
-
-    }
+    const setShow = () => {
+        setShowFree(!showFree);
+    };
 
     return (
         <div>
             <section className="py-5">
+                <Category></Category>
                 <fieldset>
                     <label>
-                        <input
-                            role="switch"
-                            type="checkbox"
-                            checked={showFree}
-                            onClick={setShow}
-                        />
+                        <input role="switch" type="checkbox" checked={showFree} onClick={setShow} />
                         <span>무료 강의</span>
                     </label>
                 </fieldset>
                 <div className="container px-4 px-lg-5 mt-5">
                     <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    {showFree ? (
-                <>{filteredLectureList.map((it) => (
-                    <LectureItem key={it.id} {...it} />
-                ))}</>
-            ) : (
-               <>{filteredLectureList2.map((it) => (
-                <LectureItem key={it.id} {...it} />
-            ))}</>
-            )}
-
-
-                        
+                        {showFree ? (
+                            <>
+                                {filteredLectureList.map((it) => (
+                                    <LectureItem key={it.id} {...it} />
+                                ))}
+                            </>
+                        ) : (
+                            <>
+                                {filteredLectureList2.map((it) => (
+                                    <LectureItem key={it.id} {...it} />
+                                ))}
+                            </>
+                        )}
                     </div>
                 </div>
             </section>
